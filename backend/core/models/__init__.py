9b43361ea478cd23418b63bccdcdabcd852416ed
h0diush <h0diush@yandex.ru>
2024-07-25 20:49:48 +0300

    create product table

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,6 +1,15 @@
-__all__ = ("db_helper", "Base", "User", "AccessToken")
+__all__ = (
+    "db_helper",
+    "Base",
+    "User",
+    "AccessToken",
+    "Category",
+    "Product",
+)
 
 from .access_token import AccessToken
+from .category import Category
 from .db_helper import db_helper
 from .base import Base
+from .product import Product
 from .user import User
---
+++ backend/core/models/product.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from core.models.mixins import IntIdMixin
+
+
+if TYPE_CHECKING:
+    from .category import Category
+
+
+class Product(Base, IntIdMixin):
+    name: Mapped[str] = mapped_column(String(20))
+    description: Mapped[str] = mapped_column(String(75))
+    price: Mapped[float]
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    category: Mapped["Category"] = relationship(back_populates="products")
