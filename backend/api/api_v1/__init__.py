f9cf9b828ec0764a19bb8252835a196a75face1d
h0diush <h0diush@yandex.ru>
2024-07-29 19:55:05 +0300

    add router /address

--- backend/api/api_v1/__init__.py
+++ backend/api/api_v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer
 
 from core.config import settings
+from .address import router as address_router
 from .auth import router as auth_router
 from .profile import router as profile_router
 from .user import router as user_router
@@ -11,6 +12,7 @@ router = APIRouter(
     prefix=settings.api.v1.prefix,
     dependencies=[Depends(http_bearer)],
 )
+router.include_router(address_router)
 router.include_router(auth_router)
 router.include_router(profile_router)
 router.include_router(user_router)
