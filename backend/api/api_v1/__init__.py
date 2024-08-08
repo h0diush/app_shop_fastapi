1d4e152c601138f6fb866c3043ccf8a0e5377302
h0diush <h0diush@yandex.ru>
2024-08-08 10:57:48 +0300

    category router init

--- backend/api/api_v1/__init__.py
+++ backend/api/api_v1/__init__.py
@@ -4,6 +4,7 @@ from fastapi.security import HTTPBearer
 from core.config import settings
 from .address import router as address_router
 from .auth import router as auth_router
+from .category import router as category_router
 from .profile import router as profile_router
 from .user import router as user_router
 
@@ -14,5 +15,6 @@ router = APIRouter(
 )
 router.include_router(address_router)
 router.include_router(auth_router)
+router.include_router(category_router)
 router.include_router(profile_router)
 router.include_router(user_router)
